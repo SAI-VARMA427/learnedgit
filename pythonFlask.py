ead49ab005d72042dec35346796891805bf9cb02
SaiVarma <saivarmapinna81@gmail.com>
2022-12-05 13:09:01 +0530

    using if statement

--- pythonFlask.py
+++ pythonFlask.py
@@ -1,5 +1,11 @@
 batcount = int(input("no.of bats sold:"))
 wicketscount = int(input("no.of wickets sold: "))
-shoescount = int(input("no.of shoes sold: "))
-totalamount = batcount*500 + wicketscount*300 + shoescount*400
-print("user need to pay:",totalamount)
+if (batcount<=0 or wicketscount<=0):
+    print("enter a positive number")
+
+# shoescount = int(input("no.of shoes sold: "))
+# totalamount = batcount*500 + wicketscount*300 + shoescount*400
+# print("user need to pay:",totalamount)
+
+# if (batcount<=0):
+#     print("enter a positive number")
