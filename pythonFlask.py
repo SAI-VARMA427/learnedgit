626bca882897fef9675f6dd3c90cab72883431cc
SaiVarma <saivarmapinna81@gmail.com>
2022-12-05 12:51:39 +0530

    firstcommit

---
+++ pythonFlask.py
@@ -0,0 +1,5 @@
+batcount = int(input("no.of bats sold:"))
+wicketscount = int(input("no.of wickets sold: "))
+shoescount = int(input("no.of shoes sold: "))
+totalamount = batcount*500 + wicketscount*300 + shoescount*400
+print("user need to pay:",totalamount)
