ff14feba170041a0053d5caa8e6040cf0d650ac8
PINNA SAI VARMA <85025248+SAI-VARMA427@users.noreply.github.com>
2022-12-07 12:21:34 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,49 @@
+#if else:
+
+# n=10
+# (if n<=0:)
+# if (n<=0):
+#     print("it is less than zero")
+# elif(n==10):
+#     print("the value is lessthan equal to five")
+# else:
+#     print("n is greater than ten")
+
+
+# students=['varma','nagu','akhil','durgesh','vishnu','ankit','ravi']
+# print('the student names are :')
+# for s in students:
+#     print(s)
+
+l=[1,2,3.2,4.55,'sai',True]
+# print(l)
+# print(l[0])
+# print(l[3])
+# print(l[1:3]) #include 1 and exclude 3
+# print(l[1:5])
+# print(l[2:]) #print all the values from 2
+# print(l*2) #print l 2times
+
+l1=[6,7,8.25,'varma',False]
+# print(l+l1) #adding l and l1
+# print(l1+l)
+
+
+""" 
+key = student name
+value = marks
+anik = 60
+{}
+object - key and value
+
+"""
+t = (1,2,3.2,4.55,'sai',True)
+print(t[0])
+print(t[1:4])
+print(t[2:])
+details = {'sai':99, 'rahul': 79,'jessi':88,'anik':100 }
+details[4]="Test Marks"
+print(details)
+print(details.values())
+print(details.keys())
+print(details[4])
