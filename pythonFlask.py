d0f7a44a488fe10d23c15216e8dfee03b516084b
PINNA SAI VARMA <85025248+SAI-VARMA427@users.noreply.github.com>
2022-12-08 11:33:23 +0530

    Add files via upload

--- pythonFlask.py
+++ pythonFlask.py
@@ -1,11 +1,28 @@
 batcount = int(input("no.of bats sold:"))
 wicketscount = int(input("no.of wickets sold: "))
-if (batcount<=0 or wicketscount<=0):
-    print("enter a positive number")
+# if (batcount<=0 or wicketscount<=0):
+#     print("enter a positive number")
 
-# shoescount = int(input("no.of shoes sold: "))
+shoescount = int(input("no.of shoes sold: "))
+ballscount = int(input("no.of balls :"))
+padscount = int(input("no.of pads: "))
+
+l = [batcount,wicketscount,shoescount,ballscount,padscount]
+# using the for loop
+
+# for i in l :
+#     print(i)
+
 # totalamount = batcount*500 + wicketscount*300 + shoescount*400
 # print("user need to pay:",totalamount)
 
-# if (batcount<=0):
-#     print("enter a positive number")
+# using the if_else statement
+if ((batcount<=0) or (wicketscount<=0) or (shoescount<=0)):
+    print("enter a positive number")
+else:
+    print('product qunatity with price :')
+    totalamount = batcount*200 + wicketscount*300 + shoescount*500
+    entries = {batcount: 200, + wicketscount: 300, + shoescount: 500}
+    for i,p in entries.items():
+        print(i,p)
+    print("user need to pay: ",totalamount)
